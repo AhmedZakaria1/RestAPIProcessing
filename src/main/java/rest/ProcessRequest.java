980440e0fab2777b468fd305d62c198d0c3d7eb9
AhmedZakaria1 <ahmed.a.zakaria@gmail.com>
2017-01-23 18:50:44 +0530

    Second Change

--- src/main/java/rest/ProcessRequest.java
+++ src/main/java/rest/ProcessRequest.java
@@ -1,76 +1,56 @@
 package rest;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-public class ProcessRequest extends RestRequest{
-	String url;
-	String requestType;
-	String outputFormat;
-	ProcessRequest(String url,String requestType,String outputFormat){
-		this.url=url;
-		this.requestType=requestType;
-		this.outputFormat=outputFormat;
-	}
-	HashMap<String,String> dataSet=new HashMap<String,String>();
-public void processRestApi(){	
-con(url);
-setRequestType(requestType);
-setOutPutMimeType(outputFormat);
-	StringBuffer sb=new StringBuffer();
-	String test;
-	String[] tkn;
+public class ProcessRequest extends RequestOutput {
+HashMap<String, String> dataSet = new HashMap<String, String>();
+
+protected void testApis(){
 	
-	try {
-		InputStream ins=conn.getInputStream();
-	InputStreamReader isr=new InputStreamReader(ins);
-	BufferedReader br=new BufferedReader(isr);
-	
-	while ( (test=br.readLine())!=null){
-		sb.append(test);
-	}
-	} catch (IOException e) {
-		e.printStackTrace();
-	}
-	String tt=sb.toString();
-	String tt1=(tt.trim().replace(" ", "").replace("[", "").replace("{", "").replace("}", ""));
-	tkn=tt1.split(":");
-	String mainToken=tkn[0];
-	tkn=tt1.split(mainToken);
-	for (String spl:tkn){
-		if (!spl.equals("")){
-		String man=mainToken+spl;
-		tkn=man.split(",");
-		dataSet.put(tkn[0].replace("\"", ""), spl);
-		//	System.out.println(man.replace(",\"", ",").replace("\":", "-"));
+	Utils util=new Utils();
+	//System.out.println(util.propertyValue("ExcelPath"));
+	ExcelUtils ExUtil = new ExcelUtils(util.propertyValue("ExcelPath"), 0);
+	String apiUrl="";
+	for (int i = 1; i <= ExUtil.numberRows(); i++) {
+		for (int k = 0; k <= 2; k++) {
+		if (ExUtil.cellData(i, k)!=null)
+			apiUrl+=ExUtil.cellData(i, k);
 		}
-	}
-	for (Map.Entry ent:dataSet.entrySet()){
-		System.out.println(ent.getKey()+" "+ent.getValue());
-	}
+		
+		setHttpconnection(apiUrl);
+		setRequestType(ExUtil.cellData(i, 3));
+		setOutPutMimeType(ExUtil.cellData(i, 4));
+		Double actual=(double) responseData(ExUtil.cellData(i, 4)).size();
+		String keyTest=ExUtil.cellData(i, 6);
+		String keyVal=ExUtil.cellData(i, 7);
+		System.out.println(keyVal+" "+checkParamData(keyTest)+" "+String.valueOf(actual)+" "+ExUtil.cellData(i, 5));
+		if ( String.valueOf(actual).equals(ExUtil.cellData(i, 5))  &&   keyVal.equals(checkParamData(keyTest))){
+			System.out.println(keyVal+" "+checkParamData(keyTest));
+			ExUtil.setcellData(i, 8, "Pass");
+		} else {
+		
+			ExUtil.setcellData(i, 8, "Fail");
+		}
+		apiUrl="";
 	}
 
-	public boolean checkData(String input){
-		for (Map.Entry ent:dataSet.entrySet()){
-		
-		if (ent.getValue().toString().contains(input))	{
-			System.out.println(ent.getKey()+" "+ent.getValue());
-		return true;
+	}
+
+	public boolean checkData(String input) {
+		for (Map.Entry ent : dataSet.entrySet()) {
+
+			if (ent.getValue().toString().contains(input)) {
+				System.out.println(ent.getKey() + " " + ent.getValue());
+				return true;
+			}
 		}
-		}	
-		
-		
+
 		return false;
-		
+
 	}
 
-	public static void main (String[] args){
-		ProcessRequest sample=new ProcessRequest("https://jsonplaceholder.typicode.com/posts", "GET", "json");
-		sample.processRestApi();
-	}
+		
 	
-}
+	}
